163f7c1020aa371bd68b08dd469b1686c790bd3a
craigrdavies <7354414+craigrdavies@users.noreply.github.com>
2026-01-18 17:02:47 +0000

    Graphic file issue updated.

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
-import logo from '../../public/icons/48x48_trim.jpg';
+import logo from 'src/48x48_trim.jpg';
 import {
   ArrowLeft,
   FileText,
